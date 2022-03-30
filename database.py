e7d162750d99b2f6fe330089d7a32e8ea984409a
Marcin Kucharski <kucharskimarcin.mk@gmail.com>
2022-03-30 21:02:03 +0200

    po wstepnych testach

--- database.py
+++ database.py
@@ -1,7 +1,39 @@
 """zbiór haseł
 """
+from password import Password
+
 class Database:
     """klasa zbioru haseł
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, path=str) -> None:
+        """tworzy obiekt klasy zbioru haseł na podstawie pliku z hasłami
+
+        Args:
+            path (str, optional): ścieżka do pliku z hasłami. Defaults to str.
+        """
+        self.path = path
+        self.passwords = self.take_passwords_from_file(self.path)
+        self.database = self.create_database(self.passwords)
+
+    @staticmethod
+    def take_passwords_from_file(path):
+        """pobiera z pliku hasła
+
+        Args:
+            path (str): ścieżka do pliku z hasłami
+
+        Returns:
+            list: lista haseł
+        """
+        passwords = []
+        with open(path, encoding='utf-8') as file:
+            for password in file:
+                passwords.append(password.strip())
+        return passwords
+
+    @staticmethod
+    def create_database(passwords):
+        database = []
+        for password in passwords:
+            database.append(Password(password))
+        return database
--- validate_passwords.py
+++ validate_passwords.py
@@ -4,5 +4,14 @@
 class ValidatePasswords:
     """klasa sprawdzająca poprawność haseł i czy wyciekło
     """
-    def __init__(self) -> None:
+    def __init__(self, database) -> None:
+        self.database = database
+
+    def take_password(self, database):
+        """pobiera hasło z bazy danych
+        """
+
+    def check_pawn(self):
         pass
+
+    
